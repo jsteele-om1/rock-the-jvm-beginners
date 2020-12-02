14863198858a0ec07805c4e91625f57a30517744
James Steele <jamessteele@ip-192-168-86-29.ec2.internal>
2020-12-02 09:43:13 -0500

    first pass at anonymous classess

--- src/exercises/MyList.scala
+++ src/exercises/MyList.scala
@@ -18,6 +18,15 @@ abstract class MyList[+A] {
   // polymorphic call
   override def toString: String = "[" + printElements + "]"
 //  override def toString: String = super.toString
+
+
+  // no idea what is happenning here
+  def map[B] (transformer: MyTransformer[A, B]): MyList[B]
+  def flatMap[B] (transformer: MyTransformer[A, MyList[B]])
+  def filter(predicate: MyPredicate[A]): MyList[A]
+
+  // concatenation
+  def ++[B>: A](list: MyList[B]): MyList[A]
 }
 
 object Empty extends MyList[Nothing] {
@@ -26,6 +35,12 @@ object Empty extends MyList[Nothing] {
   def isEmpty: Boolean = true
   def add[B >: Nothing](element: B): MyList[B] = new Cons(element, Empty)
   def printElements: String = ""
+
+  def map[B] (transformer: MyTransformer[Nothing, B]): MyList[B] = Empty
+  def flatMap[B] (transformer: MyTransformer[Nothing, MyList[B]]) = Empty
+  def filter(predicate: MyPredicate[Nothing]): MyList[Nothing] = Empty
+
+  def ++[B>: Nothing](list: MyList[B]): MyList[Nothing] = Empty
 }
 
 class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
@@ -36,6 +51,29 @@ class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
   def printElements: String =
     if (t.isEmpty) "" + h
     else h + " " + t.printElements
+
+  def map[B] (transformer: MyTransformer[A, B]): MyList[B] =
+    new Cons(transformer.transform(h), t.map(transformer))
+  def flatMap[B] (transformer: MyTransformer[Nothing, MyList[B]]) = Empty
+  def filter(predicate: MyPredicate[A]): MyList[A] =
+    if (predicate.test(h)) new Cons(h, t.filter(predicate))
+    else t.filter(predicate)
+
+  // if I concat [1,2] ++ [[3,4,5] = new Cons(1, [2] ++ [3, 4, 5]) - remember recursion
+  def ++[B>: A](list: MyList[B]): MyList[A] = new Cons(h, t ++ list)
+
+  def flatMap[B] (transformer: MyTransformer[A, MyList[B]]): MyList[B] =
+    transformer.transform(h) ++ t.flatMap(transformer) // why isn't this working
+
+
+}
+
+trait MyPredicate[-T] {
+  def test(element: T): Boolean
+}
+
+trait MyTransformer[-A, B] {
+  def transform(element: A): B
 }
 
 object ListTest extends App {
@@ -50,4 +88,13 @@ object ListTest extends App {
   println(list.isEmpty)
   // polymorphic call
   println(list2.toString)
+
+  println(list2.map(new MyTransformer[Int, Int] {
+    override def transform(element: Int): Int = element * 2
+  }).toString)
+
+  println(list2.filter(new MyPredicate[Int] {
+    override def test(element: Int): Boolean = element % 2 ==0
+  })toString)
+
 }
