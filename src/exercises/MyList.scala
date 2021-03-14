4d169d0ad25ac128321d553a490415c6db5cc5ea
James Steele <jsteele@om1.com>
2021-03-14 11:34:34 -0400

    completed whats a function really

--- src/exercises/MyList.scala
+++ src/exercises/MyList.scala
@@ -23,10 +23,10 @@ abstract class MyList[+A] {
   // polymorphic call
   override def toString: String = "[" + printElements + "]"
 
-  def map[B](transformer: MyTransformer[A, B]): MyList[B]
-
-  def flatMap[B] (transformer: MyTransformer[A, MyList[B]]): MyList[B]
-  def filter(predicate: MyPredicate[A]): MyList[A]
+  // higher-order functions - use functions as first class values
+  def map[B](transformer: A => B): MyList[B]
+  def flatMap[B] (transformer: A => MyList[B]): MyList[B]
+  def filter(predicate: A => Boolean): MyList[A]
 
   //  concatenation
   def ++[B >: A] (list: MyList[B]): MyList[B]
@@ -43,9 +43,9 @@ case object Empty extends MyList[Nothing] {
 
   def printElements: String = ""
 
-  def map[B](transformer: MyTransformer[Nothing, B]): MyList[B] = Empty
-  def flatMap[B] (transformer: MyTransformer[Nothing, MyList[B]]): MyList[B] = Empty
-  def filter(predicate: MyPredicate[Nothing]): MyList[Nothing] = Empty
+  def map[B](transformer: Nothing => B): MyList[B] = Empty
+  def flatMap[B] (transformer: Nothing => MyList[B]): MyList[B] = Empty
+  def filter(predicate: Nothing => Boolean): MyList[Nothing] = Empty
 
   def ++[B >: Nothing] (list: MyList[B]): MyList[B] = list
 }
@@ -63,17 +63,17 @@ case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
     if (t.isEmpty) "" + h
     else h + " " + t.printElements
 
-  def filter(predicate: MyPredicate[A]): MyList[A] =
-    if (predicate.test(h)) new Cons(h, t.filter(predicate))
+  def filter(predicate: A => Boolean): MyList[A] =
+    if (predicate(h)) new Cons(h, t.filter(predicate))
     else t.filter(predicate)
 
-  def map[B](transformer: MyTransformer[A, B]): MyList[B] =
-    new Cons(transformer.transform(h), t.map(transformer))
+  def map[B](transformer: A => B): MyList[B] =
+    new Cons(transformer(h), t.map(transformer))
 
   def ++[B >: A] (list: MyList[B]): MyList[B] = new Cons(h, t ++ list)
 
-  def flatMap[B] (transformer: MyTransformer[A, MyList[B]]): MyList[B] =
-    transformer.transform(h) ++ t.flatMap(transformer)
+  def flatMap[B] (transformer: A => MyList[B]): MyList[B] =
+    transformer(h) ++ t.flatMap(transformer)
 }
 
 // removing for now
@@ -102,18 +102,18 @@ object ListTest extends App {
 
   //  println(list1.m)
 
-  println(list1.map(new MyTransformer[Int, Int] {
-    override def transform(elem: Int): Int = elem * 2
+  println(list1.map(new Function1[Int, Int] {
+    override def apply(elem: Int): Int = elem * 2
   }).toString)
 
-  println(list1.filter(new MyPredicate[Int] {
-    override def test(elem: Int): Boolean = elem % 2 == 0
+  println(list1.filter(new Function1[Int, Boolean] {
+    override def apply(elem: Int): Boolean = elem % 2 == 0
   }).toString)
 
   println((list1 ++ anotherList).toString)
   println("test flatmap")
-  println(list1.flatMap(new MyTransformer[Int, MyList[Int]] {
-    override def transform(elem: Int): MyList[Int] = new Cons(elem, new Cons(elem + 1, Empty))
+  println(list1.flatMap(new Function1[Int, MyList[Int]] {
+    override def apply(elem: Int): MyList[Int] = new Cons(elem, new Cons(elem + 1, Empty))
   }).toString)
 
   /// after case class implementation
--- src/lectures/part3fp/WhatsAFunction.scala
+++ src/lectures/part3fp/WhatsAFunction.scala
@@ -35,9 +35,21 @@ object WhatsAFunction extends App {
   def concatenator: (String, String) => String = new Function2[String, String, String] {
     override def apply(a: String, b: String): String = a + b
   }
-
   println(concatenator("hello ", "scala"))
 
+  // Function1[Int, Function1 [Int, Int]]
+  val superAdder: Function1[Int, Function1 [Int, Int]] = new Function1[Int, Function1 [Int, Int]] {
+    override def apply(x: Int): Function1[Int, Int] = new Function1[Int, Int] {
+      override def apply(y: Int): Int = x + y
+    }
+  }
+
+  val adder3 = superAdder(3) // this val is a new function
+  println(adder3(4)) // implementation is the nested override
+  println(superAdder(3)(4)) // returns the same thing as above - called a curried function
+
+  //use functions as first class elements - work with functions just like any other values
+  // all scala functions are objects!
 
 
 }
